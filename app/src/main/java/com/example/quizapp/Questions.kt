e9d7165fc8cf881b2c4863caab35d5e0355e9130
CAESERar <55337596+CAESERar@users.noreply.github.com>
2020-05-28 13:22:16 +0530

    Quiz App

---
+++ app/src/main/java/com/example/quizapp/Questions.kt
@@ -0,0 +1,13 @@
+package com.example.quizapp
+
+data class Questions (
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val OptionOne: String,
+    val OptionTwo: String,
+    val OptionThree: String,
+    val OptionFour: String,
+    val correctOption: Int
+
+)
